c205eea2ec734f8a0649d1e41865b581f221fafd
LilyWS <stranger1212ss23@gmail.com>
2021-11-01 11:55:36 -0400

    more pretty colors

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get(('/contact'), (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/contact.html"));
+});
+
+app.get(('/projects'), (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/projects.html"));
+});
+
 app.get(('/*'), (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
